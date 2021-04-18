b833c3901edd86953905939bf77617ca19134eae
Gilmar Pereira de Alcantara <gilmar.alcantara@HOTBHZA002129.Dlink>
2021-04-17 21:46:38 -0300

    create heath controller

---
+++ application/src/main/kotlin/com/hotmart/baseapi/application/controller/HealthyController.kt
@@ -0,0 +1,12 @@
+package com.hotmart.baseapi.application.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/")
+class HealthyController {
+    @GetMapping
+    fun get() = "health"
+}
