945f712b47605b95adf1dde763b8cc1917c1966a
Gilmar Pereira de Alcantara <gilmar.alcantara@HOTBHZA002129.Dlink>
2021-04-22 22:26:38 -0300

    craete application bean for application

--- usecase/src/main/kotlin/com/hotmart/baseapi/usecase/person/IGetAllPersonsUseCase.kt
+++
@@ -1,6 +0,0 @@
-package com.hotmart.baseapi.usecase.person
-
-import com.hotmart.baseapi.usecase.base.IUseCaseOnlyResponse
-import com.hotmart.baseapi.usecase.person.response.PersonResponse
-
-interface IGetAllPersonsUseCase: IUseCaseOnlyResponse<Collection<PersonResponse>>
