f1ce1c627ebab6d3bd2bd61abf431f0ec76859ed
Gilmar Pereira de Alcantara <gilmar.alcantara@HOTBHZA002129.Dlink>
2021-04-25 23:01:55 -0300

    createperson controller test using restcontroller

--- application/src/test/kotlin/com/hotmart/baseapi/application/resttemplate/PersonControllerTest.kt
+++ application/src/test/kotlin/com/hotmart/baseapi/application/resttemplate/PersonControllerTest.kt
@@ -1,11 +1,13 @@
 package com.hotmart.baseapi.application.resttemplate
 
-import com.hotmart.baseapi.usecase.base.UseCaseResponse
+import com.hotmart.baseapi.usecase.person.request.CreatePersonRequest
+import com.hotmart.baseapi.usecase.person.response.PersonResponse
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpEntity
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.test.context.jdbc.Sql
@@ -16,11 +18,28 @@ class PersonControllerTest {
     private lateinit var restTemplate: TestRestTemplate
 
     @Test
-         @Sql("/test.sql")
-    fun `Should healthy`() {
+    @Sql("/test.sql")
+    fun `Should get persons`() {
+        val response: ResponseEntity<List<*>> = restTemplate.getForEntity("/persons", List::class.java)
 
-//        val response: ResponseEntity<*> = restTemplate.getForEntity("/")
+        Assertions.assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+        Assertions.assertThat(response.body).hasSize(3)
 
-//        Assertions.assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+//        val data = ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(response.body)
+//        println(data)
+    }
+
+    @Test
+    fun `Should post person`() {
+        val person = CreatePersonRequest("Gilmar")
+        val entity = HttpEntity<CreatePersonRequest>(person)
+
+        val response: ResponseEntity<PersonResponse> =
+            restTemplate.postForEntity("/persons", entity, PersonResponse::class.java)
+
+        Assertions.assertThat(response.statusCode).isEqualTo(HttpStatus.CREATED)
+        Assertions.assertThat(response.body)
+            .hasFieldOrProperty("id")
+            .hasFieldOrPropertyWithValue("name", "Gilmar")
     }
 }
